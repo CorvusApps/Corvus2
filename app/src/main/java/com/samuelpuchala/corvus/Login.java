d6d6d9bdc71e0fad11c1b2d34a47226c661e437f
SamuelPuchala <sam@satov.ca>
2019-06-01 19:41:22 -0400

    connected to firebase and set up for facebook login - java code still to come

--- app/src/main/java/com/samuelpuchala/corvus/Login.java
+++ app/src/main/java/com/samuelpuchala/corvus/Login.java
@@ -1,7 +1,15 @@
 package com.samuelpuchala.corvus;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+       // printKeyHash();
+
     }
+
+//    private void printKeyHash() {
+//
+//        try {
+//
+//            PackageInfo info = getPackageManager().getPackageInfo("com.samuelpuchala.corvus",
+//                    PackageManager.GET_SIGNATURES);
+//
+//            for (Signature signature:info.signatures) {
+//
+//                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+//                messageDigest.update(signature.toByteArray());
+//                Log.e("KEYHASH", Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT));
+//
+//            }
+//
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
