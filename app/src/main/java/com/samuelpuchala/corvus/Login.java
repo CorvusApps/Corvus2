eee2d750bdb268b5f6752c92b793eeead7accc60
SamuelPuchala <sam@satov.ca>
2019-06-02 07:53:47 -0400

    Facebook login complete

--- app/src/main/java/com/samuelpuchala/corvus/Login.java
+++ app/src/main/java/com/samuelpuchala/corvus/Login.java
@@ -1,28 +1,126 @@
 package com.samuelpuchala.corvus;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    FirebaseAuth firebaseAuth;
+    CallbackManager callbackManager;
+
+    LoginButton btnFBLoginX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
        // printKeyHash();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+
+        btnFBLoginX = (LoginButton)findViewById(R.id.btnFBLogin);
+        btnFBLoginX.setReadPermissions("email");
+        btnFBLoginX.setOnClickListener(this);
 
     }
 
-//    private void printKeyHash() {
+    @Override
+    public void onClick(View view) {
+       // Toast.makeText(Login.this, "button works", Toast.LENGTH_LONG).show();
+
+        switch (view.getId()){
+
+            case R.id.btnFBLogin:
+
+                signIn();
+
+                break;
+
+        }
+
+    }
+
+    private void signIn() {
+
+        btnFBLoginX.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                Log.e("ERROR_COMT", ""+e.getMessage());
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                String email = authResult.getUser().getEmail();
+                Toast.makeText(Login.this, "You are signed in with email: " + email, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //    private void printKeyHash() {
 //
 //        try {
 //
