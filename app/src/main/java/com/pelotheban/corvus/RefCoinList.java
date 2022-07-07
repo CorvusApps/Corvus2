f9050846f89fb9f5b4fc22db6651e61bc67a13ae
Samuel Puchala <serbitar45@hotmail.com>
2022-07-06 22:53:42 -0400

    try 3

--- app/src/main/java/com/pelotheban/corvus/RefCoinList.java
+++ app/src/main/java/com/pelotheban/corvus/RefCoinList.java
@@ -66,7 +66,7 @@ public class RefCoinList extends AppCompatActivity implements RewardedVideoAdLis
     private TextView txtRefCListCollUIDX;
     private String cRefListuid;
     private int era;
-     // temp note
+     // temp note2
     // Firebase related
     private FirebaseAuth firebaseAuthRefCoins;
     private DatabaseReference coinRefDatabase;
